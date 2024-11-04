191f933495429e46e1780139e35eecd8d9a69e81
SimonGrFo <simgrafor03@gmail.com.>
2024-11-04 18:33:03 +0100

    styling

--- src/main/java/com/example/jambo/Jambo.java
+++ src/main/java/com/example/jambo/Jambo.java
@@ -3,7 +3,10 @@ package com.example.jambo;
 import com.example.jambo.controllers.JamboController;
 import com.example.jambo.ui.JamboUI;
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.util.Objects;
 
 public class Jambo extends Application {
     @Override
@@ -11,6 +14,10 @@ public class Jambo extends Application {
         JamboUI ui = new JamboUI();
         JamboController controller = new JamboController(ui);
         controller.initializeStage(primaryStage);
+        Scene scene = ui.createScene(controller);
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/styles/style.css")).toExternalForm());
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
